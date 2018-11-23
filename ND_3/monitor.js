7ac9a8aff5f7bba028c1787008ff63c2e117d5ba
kamile385 <k.radlinskaite@gmail.com>
2018-11-23 19:13:09 +0200

    Added memory percentage

--- ND_3/monitor.js
+++ ND_3/monitor.js
@@ -4,11 +4,10 @@ setInterval(function() {
     si.mem().then(data => {
         console.log('MEMORY in bytes:');
         console.log('Total        :', data.total);
-        console.log('Used         :', data.used);
-        console.log('Not used     :', data.free);
+        console.log('Free         :', data.free);
 
-        let CPUpercentage = (Math.floor((data.used / data.total) * 100));
-        console.log(CPUpercentage + '%');
+        let memoryPercentage = (Math.floor((data.used / data.total) * 100));
+        console.log('Used         : ' + memoryPercentage + '%');
         console.log('.......................................');
     })
 }, 500)
