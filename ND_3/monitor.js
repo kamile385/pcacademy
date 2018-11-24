80d1d58d5348aea4cdb8df90b33287cf0e892b0e
kamile385 <k.radlinskaite@gmail.com>
2018-11-24 23:41:18 +0200

    Showing CPU usage

--- ND_3/monitor.js
+++ ND_3/monitor.js
@@ -1,13 +1,19 @@
 const si = require('systeminformation');
+let os 	= require('os-utils');
 
-setInterval(function() {
+let time_interval = 500;
+
+setInterval(function() { 
     si.mem().then(data => {
         console.log('MEMORY in bytes:');
         console.log('Total        :', data.total);
         console.log('Free         :', data.free);
 
         let memoryPercentage = (Math.floor((data.used / data.total) * 100));
-        console.log('Used         : ' + memoryPercentage + '%');
-        console.log('.......................................');
-    })
-}, 500)
+        console.log('Used         : ' + memoryPercentage + ' %');
+    });
+    os.cpuUsage(function(v){
+        console.log('CPU Usage    : ' + (Math.floor(v * 100)) + ' %');
+    });
+    console.log('...........................');
+}, time_interval)
